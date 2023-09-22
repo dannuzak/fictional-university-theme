fe754db8e0987bb6a2e6db9ff15307d4ed85555b
Daniela Zacarias <daniela.zacarias@outlook.com>
2023-09-22 19:23:36 +0100

    added images for the slide and the script file to make it work

--- functions.php
+++ functions.php
@@ -2,6 +2,10 @@
 
 function university_files() {
     //here we can load CSS and JS files
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+    //third argument is an array of dependencies because it needs to know if there are any dependencies that need to be loaded before this file. In this case we want to load jQuery before our custom JS file. We can find the name of the jQuery file in the WP documentation.If there is no dependency, we set the value to NULL
+    //fourth argument is the version of the file, we can put whatever we want, but it's a good practice to put the version of the theme. 
+    //last argument is a boolean that tells WP if we want to load the file in the header or in the footer. True means in the footer, false means in the header
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
