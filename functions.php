c42ed1e478b925ec3e7a4d7c571b9091d55e7c3d
Daniela Zacarias <daniela.zacarias@outlook.com>
2023-09-22 18:29:30 +0100

    added lines of html, css, fonts and social media icons

--- functions.php
+++ functions.php
@@ -2,7 +2,10 @@
 
 function university_files() {
     //here we can load CSS and JS files
-    wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
     //first argument is the name of the style, the name doesn't matter, it's just a name to identify the style
     //second argument is a WP function that will return the path to the style.css file
 }
